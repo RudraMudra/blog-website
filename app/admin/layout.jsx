cc30594ba9368707d5859ad1c7a1a95419a0ae89
TYRANNY905 <93469473+RudraMudra@users.noreply.github.com>
2024-11-15 17:27:49 +0530

    removed authcontexxt from admin/layout

--- app/admin/layout.jsx
+++ app/admin/layout.jsx
@@ -1,37 +1,10 @@
-"use client";
-
-import AuthProvider from "@/lib/contexts/AuthContext";
 import Sidebar from "./components/Sidebar";
-import { useAdmin } from "@/lib/firebase/admins/read";
 
 export default function Layout({ children }) {
-  return <AuthProvider>
-      <InnerLayout>{children}</InnerLayout>
-  </AuthProvider>
-}
-
-function InnerLayout({ children }) {
-//   const { user, isLoading: authIsLoading } = useAuth();
-//   const { data, error, isLoading } = useAdmin({ uid: user?.uid });
-//   if (authIsLoading || isLoading) {
-//     return <h2>Loading ...</h2>;
-//   }
-//   if (error) {
-//     return <p>{error}</p>;
-//   }
-//   if (!data) {
-//     return (
-//       <div>
-//         <h1>You are not admin</h1>
-//       </div>
-//     );
-//   }
   return (
-    <>
-      <section className="flex">
-        <Sidebar />
-        {children}
-      </section>
-    </>
+    <section className="flex">
+      <Sidebar />
+      {children}
+    </section>
   );
 }
